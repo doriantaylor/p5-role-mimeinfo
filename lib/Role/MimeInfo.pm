68599986ac73efc5f769932327fa6236bf78c892
Dorian Taylor <dorian.taylor.lists@gmail.com>
2017-09-20 12:12:23 -0700

    duh forgot to add namespace::autoclean to makefile

--- lib/Role/MimeInfo.pm
+++ lib/Role/MimeInfo.pm
@@ -20,11 +20,11 @@ Role::MimeInfo - Bolt-on type checking against GNOME shared-mime-info
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
